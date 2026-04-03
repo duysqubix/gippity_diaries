0cfc899ceccfcb0bc43e6dbad9a6afce57a892aa
duysqubix <duys@qubixds.com>
2026-04-03 10:50:20 -0500

    feat: respect PORT env var when running locally

--- app/app.py
+++ app/app.py
@@ -70,4 +70,5 @@ def events():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=False, threaded=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=False, threaded=True)
